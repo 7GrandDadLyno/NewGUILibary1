fe81327f017b6d48d1b3d174213dfc3f28a81fb6
7GrandDadVape <127914282+7GrandDadLyno@users.noreply.github.com>
2023-03-31 15:46:08 +0800

    Create TEST1.lua

---
+++ NeedAntiCheatByPass/WalkSpeed50/TEST1.lua
@@ -0,0 +1,4 @@
+local DevEnv = loadstring(game:HttpGet('https://raw.githubusercontent.com/Extorius/Developer-Utilities/main/src/library.lua'))({
+	['cache-event'] = false -- whether to fire the example cache-event after loading in
+})
+DevEnv.F:ChangeWalkSpeed(50)
